4534df5288cd5f4cf083a17e1b7c7d8e0a4ec88a
Joe <joembatia270@gmail.com>
2024-06-19 15:42:37 +0300

    Changed the folder name for my script files

--- src/script.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.scroll-to').forEach(item => {
-    item.addEventListener('click', function(event) {
-        event.preventDefault();
-        const targetId = this.getAttribute('href');
-        document.querySelector(targetId).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
